4041da299759ca1fcb2ef8b2602e823abfd87d7f
fjl <fengjianglin6817@dingtalk.com>
2018-05-28 17:32:05 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const http=require('http');
+const fs=require('fs');
+const qs=require('fs');
+const urlLib=require('url');
+const app=http.createServer(function (req,res) {
+
+});
+app.listen(8080);
